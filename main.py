bdafe26fc8894a5a427349b00d21db745588bac4
FalconFuryX77 <praneel77paliwal@gmail.com>
2025-08-19 18:29:36 +0000

    Made Asteroids

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     tc = pygame.time.Clock()
     dt = 0
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Player.containers = (updatable,drawable) #containers are always on class not instance
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
